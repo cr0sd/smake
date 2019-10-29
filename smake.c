79f2beb1b31c2f7e5f2c11ec4f9b4ff8e103ec46
cr0sd <coreydunn92@gmail.com>
2019-10-28 20:05:39 -0500

    Technically complete

--- smake.c
+++ smake.c
@@ -10,6 +10,9 @@ int main()
 	char*c=NULL;
 	char*d=NULL;
 	char e[512];
+	char*drest=NULL;
+	char cmd[1024]={0};
+	char t[512];
 
 	// Open file called (( CWD + '\' + makefile ))
 	strcpy(e,b);
@@ -17,9 +20,32 @@ int main()
 	c=filestring(e);
 
 	// Tokenize string
-	d=strtok(c," \n\t");
-	do puts(d);
-	while(d=strtok(NULL," \n\t"));
+	drest=c;
+	while(d=strtok_r(drest," \n\t",&drest))
+	{
+		if(d[strlen(d)-1]==':')
+		{
+			//printf("\n%s\n",d);
+			//puts("----------");
+
+			// Skip if no command
+			if(*cmd==0)continue;
+
+			// Run command
+			system(cmd);
+			printf("running '%s'\n",cmd);
+			*cmd=0;
+		}
+		else
+		{
+			sprintf(t,"%s ",d);
+			strcat(cmd,t);
+			//printf("%s ",d);
+		}
+		
+		
+	}
+
 
 	// Free resources
 	free(b);
