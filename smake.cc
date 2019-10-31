b3c09956b1f7724dbde04ae9b73b56f11e9932f3
cr0sd <coreydunn92@gmail.com>
2019-10-31 17:45:37 -0500

    Consolidated some code

--- smake.cc
+++ smake.cc
@@ -6,9 +6,27 @@
 #include<stack>
 #include<map>
 
-//#define WINDOWS
 #define PROG_NAME "smake"
-#include"platform.h"
+//#define WINDOWS
+
+#if defined(WINDOWS)
+	#define SLASH "\\"
+	#include<windows.h>
+	#define getcwd(x,y) GetCurrentDirectory(y,x)
+#else
+	#define SLASH "/"
+	#include<unistd.h> // For getcwd
+#endif
+
+std::string cwd()
+{
+	char b[512];
+
+	getcwd(b,512);
+
+	std::string s=b;
+	return s;
+}
 
 int main(int argc,char**argv)
 {
